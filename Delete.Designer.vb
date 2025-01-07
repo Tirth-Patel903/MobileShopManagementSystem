556f49ef3dfe115fd53d6b0d57ff0705a82ba33f
Tirth Patel <99315530+Tirth-Patel903@users.noreply.github.com>
2025-01-07 09:09:37 -0500

    Add files via upload

---
+++ Delete.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Delete
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(353, 190)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 34)
+        Me.Button1.TabIndex = 78
+        Me.Button1.Text = "Delete"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Customer", "Mobile"})
+        Me.ComboBox2.Location = New System.Drawing.Point(353, 75)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(134, 24)
+        Me.ComboBox2.TabIndex = 77
+        Me.ComboBox2.Text = "--Select--"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(263, 144)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(61, 16)
+        Me.Label15.TabIndex = 76
+        Me.Label15.Text = "Enter Id"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(353, 141)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(134, 22)
+        Me.TextBox1.TabIndex = 75
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.Label3.Location = New System.Drawing.Point(249, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(281, 25)
+        Me.Label3.TabIndex = 74
+        Me.Label3.Text = "Delete Mobile Information"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(272, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 16)
+        Me.Label1.TabIndex = 73
+        Me.Label1.Text = "Select"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 235)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(751, 281)
+        Me.DataGridView1.TabIndex = 79
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.MobileShopManagementSystem.My.Resources.Resources.kashipara
+        Me.PictureBox1.Location = New System.Drawing.Point(616, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(147, 72)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 80
+        Me.PictureBox1.TabStop = False
+        '
+        'Delete
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(775, 528)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Delete"
+        Me.Text = "Delete"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label15 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Delete.vb
@@ -0,0 +1,97 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class Delete
+    Dim con As New SqlConnection
+    Dim da As New SqlDataAdapter
+    Dim com As SqlCommand
+    Dim ds As New DataSet
+    Dim dt As DataTable
+    Dim dv As DataView
+    Dim str As String
+    Dim mobile As Object
+    Dim customer As Object
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox2.SelectedIndex = "0" Then
+            Try
+                con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\dell\documents\visual studio 2015\Projects\MobileShopManagementSystem\MobileShopManagementSystem\Mobile.mdf;Integrated Security=True")
+                con.Open()
+                str = "DELETE FROM customer WHERE id = '" & TextBox1.Text & "'"
+
+                com = New SqlCommand(str, con)
+                com.ExecuteNonQuery()
+                con.Close()
+                MsgBox(" customer Record Delete Successfully")
+                Using con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\dell\documents\visual studio 2015\Projects\MobileShopManagementSystem\MobileShopManagementSystem\Mobile.mdf;Integrated Security=True")
+                    str = "SELECT * FROM customer"
+                    com = New SqlCommand(str, con)
+                    da = New SqlDataAdapter(com)
+                    dt = New DataTable()
+                    dv = New DataView()
+                    da.Fill(dt)
+
+                    DataGridView1.DataSource = New BindingSource(dt, customer)
+                    Hide()
+                End Using
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+
+        ElseIf ComboBox2.SelectedIndex = "1" Then
+            Try
+                con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\dell\documents\visual studio 2015\Projects\MobileShopManagementSystem\MobileShopManagementSystem\Mobile.mdf;Integrated Security=True")
+                con.Open()
+                str = "DELETE FROM mobile WHERE id = '" & TextBox1.Text & "'"
+
+                com = New SqlCommand(str, con)
+                com.ExecuteNonQuery()
+                con.Close()
+                MsgBox(" Mobile Record Delete Successfully")
+                Using con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\dell\documents\visual studio 2015\Projects\MobileShopManagementSystem\MobileShopManagementSystem\Mobile.mdf;Integrated Security=True")
+                    str = "SELECT * FROM mobile"
+                    com = New SqlCommand(str, con)
+                    da = New SqlDataAdapter(com)
+                    dt = New DataTable()
+                    dv = New DataView()
+                    da.Fill(dt)
+
+                    DataGridView1.DataSource = New BindingSource(dt, mobile)
+                    Hide()
+                End Using
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+        TextBox1.Text = ""
+        ComboBox2.Text = "--Select--"
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+        If ComboBox2.SelectedIndex = "0" Then
+            Using con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\dell\documents\visual studio 2015\Projects\MobileShopManagementSystem\MobileShopManagementSystem\Mobile.mdf;Integrated Security=True")
+                str = "SELECT * FROM customer "
+                com = New SqlCommand(str, con)
+                da = New SqlDataAdapter(com)
+                dt = New DataTable()
+                dv = New DataView()
+                da.Fill(dt)
+
+                DataGridView1.DataSource = New BindingSource(dt, customer)
+            End Using
+        ElseIf ComboBox2.SelectedIndex = "1" Then
+            Using con = New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\dell\documents\visual studio 2015\Projects\MobileShopManagementSystem\MobileShopManagementSystem\Mobile.mdf;Integrated Security=True")
+                str = "SELECT * FROM mobile"
+                com = New SqlCommand(str, con)
+                da = New SqlDataAdapter(com)
+                dt = New DataTable()
+                dv = New DataView()
+                da.Fill(dt)
+
+                DataGridView1.DataSource = New BindingSource(dt, mobile)
+            End Using
+        End If
+    End Sub
+End Class
